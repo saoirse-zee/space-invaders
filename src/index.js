dd227e17fca7ba38bb5d42493f041416ef10eb24
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-07-25 17:16:38 -0400

    Set up game framework.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
-import { component } from './component.js'
+const { requestAnimationFrame } = window
 
-const config = {
-    buttonLabel: 'hey there',
+let state = {
+    userAction: '',
+    clock: 0,
+    laser: 0,
+    invader: {
+        lastMove: Date.now(),
+        position: 0,
+    },
 }
 
-function rootComponent() {
-    return component(config)
+function step() {
+    requestAnimationFrame(function() {
+        // update
+        state = update(state);
+        // render
+        render(state);
+        requestAnimationFrame(step) // ?
+    })
 }
 
-document.body.appendChild(rootComponent())
+// Handlers
+document.body.addEventListener('keydown', function(event) {
+    console.log(event.keyCode)
+    if (event.keyCode === 37) {
+        state.userAction = 'left'
+    }
+    if (event.keyCode === 39) {
+        state.userAction = 'right'
+    }
+    if (event.keyCode === 32) {
+        state.userAction = 'fire'
+    }
+})
+
+
+step()
+
+function update(state) {
+    let userAction = state.userAction
+    
+    let laser = state.laser
+    if (state.userAction === 'right') {
+        laser = laser + 1
+        userAction = ''
+    }
+    if (state.userAction === 'left') {
+        laser = laser - 1
+        userAction = ''
+    }
+
+    let invader = state.invader
+    const now = Date.now()
+    if (now - state.invader.lastMove > 1000) {
+        invader = {
+            lastMove: now,
+            position: state.invader.position + 1
+        }
+    }
+
+    const nextState = {...state, laser, userAction, invader};
+    return nextState
+}
+
+function render(state) {
+    document.body.innerText = JSON.stringify(state, null, 2)
+}
