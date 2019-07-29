f87f85f08906cef350243fb2e04b5c1516d4df5e
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-07-28 21:08:08 -0400

    🧹 Lint stuff in the base dir too!
    Oops, missed these last time.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-import { render } from './render'
-import { canvas } from './html/canvas'
-import { logger } from './html/logger'
-import { update } from './update'
-import { initState } from './init'
-import { CANVAS_HEIGHT, CANVAS_WIDTH } from './config'
+import { render } from "./render";
+import { canvas } from "./html/canvas";
+import { logger } from "./html/logger";
+import { update } from "./update";
+import { initState } from "./init";
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./config";
 
-let state = initState()
+let state = initState();
 
-document.body.style.backgroundColor = 'black'
-document.body.appendChild(canvas(CANVAS_WIDTH, CANVAS_HEIGHT))
-document.body.appendChild(logger())
+document.body.style.backgroundColor = "black";
+document.body.appendChild(canvas(CANVAS_WIDTH, CANVAS_HEIGHT));
+document.body.appendChild(logger());
 
-const { requestAnimationFrame } = window
-let lastStepTimestamp = 0
-let delta = 0
-const TIMESTEP = 1000 / 60
+const { requestAnimationFrame } = window;
+let lastStepTimestamp = 0;
+let delta = 0;
+const TIMESTEP = 1000 / 60;
 
 function step(timestamp = 0) {
-    delta += timestamp - lastStepTimestamp
-    lastStepTimestamp = timestamp
-    while(delta >= TIMESTEP) {
-        state = update(state, TIMESTEP);
-        delta -= TIMESTEP
-    }
-    render(state);
-    requestAnimationFrame(step)
+  delta += timestamp - lastStepTimestamp;
+  lastStepTimestamp = timestamp;
+  while (delta >= TIMESTEP) {
+    state = update(state, TIMESTEP);
+    delta -= TIMESTEP;
+  }
+  render(state);
+  requestAnimationFrame(step);
 }
 
 // Handlers
-document.body.addEventListener('keydown', function(event) {
-    if (event.keyCode === 37) {
-        state.userAction = 'left'
-    }
-    if (event.keyCode === 39) {
-        state.userAction = 'right'
-    }
-    if (event.keyCode === 32) {
-        state.userAction = 'fire'
-    }
-})
+document.body.addEventListener("keydown", function(event) {
+  if (event.keyCode === 37) {
+    state.userAction = "left";
+  }
+  if (event.keyCode === 39) {
+    state.userAction = "right";
+  }
+  if (event.keyCode === 32) {
+    state.userAction = "fire";
+  }
+});
 
-step()
+step();
--- src/init.js
+++ src/init.js
@@ -1,55 +1,55 @@
-import { INVADER_SPACING, LASER_POSITION_Y } from './config'
+import { INVADER_SPACING, LASER_POSITION_Y } from "./config";
 
 export function initState() {
-    return {
-        userAction: '',
-        clock: 0,
-        laser: {
-          position: [0, LASER_POSITION_Y],
-          vx: 0
-        },
-        invaders: initInvaders(),
-        missiles: [],
-        invaderVelocity: 0.005,
-    };
+  return {
+    userAction: "",
+    clock: 0,
+    laser: {
+      position: [0, LASER_POSITION_Y],
+      vx: 0
+    },
+    invaders: initInvaders(),
+    missiles: [],
+    invaderVelocity: 0.005
+  };
 }
 
 function initInvaders() {
-  const invaders = []
-  
+  const invaders = [];
+
   for (let i = 0; i < 5; i++) {
-    const y = INVADER_SPACING * i
+    const y = INVADER_SPACING * i;
     const offset = i / 500; // To make the velocity switch work 🤷🏻‍♀️
     for (let j = 1; j <= 5; j++) {
-      const x = INVADER_SPACING * j
+      const x = INVADER_SPACING * j;
       invaders.push({
-        position: [x + offset,y],
+        position: [x + offset, y],
         alive: true,
         type: getType(i)
-      })
+      });
       invaders.push({
-        position: [-x + offset,y],
+        position: [-x + offset, y],
         alive: true,
         type: getType(j)
-      })
+      });
     }
     invaders.push({
-      position: [offset,y],
+      position: [offset, y],
       alive: true,
       type: getType(i)
-    })
+    });
   }
-  
-  return invaders
+
+  return invaders;
 }
 
 function getType(rowIndex) {
   const mapRowToType = {
-    1: 'small',
-    2: 'medium',
-    3: 'medium',
-    4: 'large',
-    5: 'large',
-  }
-  return mapRowToType[rowIndex]
+    1: "small",
+    2: "medium",
+    3: "medium",
+    4: "large",
+    5: "large"
+  };
+  return mapRowToType[rowIndex];
 }
