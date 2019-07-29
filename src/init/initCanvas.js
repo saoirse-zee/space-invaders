a9fdeb416a111330668dc87a1a484b35f9a0a52b
Emma Smith <saoirse-zee@users.noreply.github.com>
2019-07-29 17:39:48 -0400

    Center the gameboard

--- src/init/initCanvas.js
+++ src/init/initCanvas.js
@@ -4,6 +4,10 @@ import { CANVAS_HEIGHT, CANVAS_WIDTH } from "../config";
 
 export function initCanvas() {
   document.body.style.backgroundColor = "black";
-  document.body.appendChild(canvas(CANVAS_WIDTH, CANVAS_HEIGHT));
+  document.body.style.display = "flex";
+  document.body.style.flexDirection = "column";
+  const gameboard = canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  gameboard.style.margin = "auto";
+  document.body.appendChild(gameboard);
   document.body.appendChild(logger());
 }
